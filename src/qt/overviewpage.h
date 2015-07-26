d39d2849c1eea58b1c3a6565e4d8f3ab1e390569
HagbardCeline <adamandshandra@server.local>
2015-07-26 14:21:37 -0600

    0.9.3-preCore

--- src/qt/overviewpage.h
+++ src/qt/overviewpage.h
@@ -49,7 +49,6 @@ private slots:
     void updateDisplayUnit();
     void handleTransactionClicked(const QModelIndex &index);
     void updateAlerts(const QString &warnings);
-    void on_pushButton_clicked();
 };
 
 #endif // OVERVIEWPAGE_H
